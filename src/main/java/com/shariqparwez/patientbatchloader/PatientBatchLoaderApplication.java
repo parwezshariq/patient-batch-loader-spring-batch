144ac97587d58abd53cfd35b1fa7faed7db60ca5
parwezshariq <shariqparwez@outlook.com>
2023-04-13 13:37:50 +0530

    Initial commit for setting up repository

---
+++ src/main/java/com/shariqparwez/patientbatchloader/PatientBatchLoaderApplication.java
@@ -0,0 +1,13 @@
+package com.shariqparwez.patientbatchloader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientBatchLoaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatientBatchLoaderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shariqparwez/patientbatchloader/PatientBatchLoaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shariqparwez.patientbatchloader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientBatchLoaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
